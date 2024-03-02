da2f295a46236f23feb5b16f9c2ff88ec0c3c9be
ZeyQuanC <ZeyQuan40@gmail.com>
2024-03-01 20:23:07 -0500

    finished project

--- app/src/main/java/com/example/bitfitp1/MainActivity.kt
+++ app/src/main/java/com/example/bitfitp1/MainActivity.kt
@@ -1,61 +1,34 @@
 package com.example.bitfitp1
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.launch
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: NutritionViewModel
-    private lateinit var adapter: NutritionEntryAdapter
-    private lateinit var mealTypeEditText: EditText
-    private lateinit var caloriesEditText: EditText
-    private lateinit var addButton: Button
-    private lateinit var recyclerView: RecyclerView
+    private lateinit var fragmentManager: FragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mealTypeEditText = findViewById(R.id.mealTypeEditText)
-        caloriesEditText = findViewById(R.id.caloriesEditText)
-        addButton = findViewById(R.id.addButton)
-        recyclerView = findViewById(R.id.recyclerView)
+        fragmentManager = supportFragmentManager
 
-        val dao = MyApp.database.nutritionEntryDao()
-        val repository = NutritionRepository(dao)
-        viewModel = ViewModelProvider(
-            this,
-            NutritionViewModelFactory(repository)
-        )[NutritionViewModel::class.java]
-
-        // Initialize RecyclerView
-        adapter = NutritionEntryAdapter()
-        recyclerView.adapter = adapter
-
-        addButton.setOnClickListener {
-            val mealType = mealTypeEditText.text.toString()
-            val calories = caloriesEditText.text.toString().toInt()
-            val entry = NutritionEntry(
-                date = System.currentTimeMillis(),
-                mealType = mealType,
-                caloriesConsumed = calories
-            )
-
-            viewModel.insertEntry(entry)
-        }
-        lifecycleScope.launch {
-            // Observe the LiveData within the activity's lifecycle
-            viewModel.getAllEntries().observe(this@MainActivity) { entries ->
-                // Update the UI with the new list of entries
-                adapter.submitList(entries)
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
+            when (item.itemId) {
+                R.id.menu_nutrition -> fragment = NutritionFragment()
+                R.id.menu_dashboard -> fragment = DashboardFragment()
             }
+            fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
+            true
         }
-    }
 
+        // Set default fragment
+        val nutritionFragment = NutritionFragment()
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, nutritionFragment).commit()
+    }
 }
